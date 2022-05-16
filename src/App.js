1db5fb7fc330a4547f785427b6faeafe9be118a7
ashutoshvish07 <101579327+ashutoshvish07@users.noreply.github.com>
2022-05-16 18:36:32 +0530

    react menu day1 assign complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+// import "./App.css";
+import List from "./components/List";
+import "./components/List.css";
+function App() {
+  return (
+    <div className="App">
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,25 @@
+const List = ()=>{
+
+    return (
+        <div className="list">
+            <h1>Mobile Operating system</h1>
+            <ul>
+                <li>Android</li>
+                <li>Blackberry</li>
+                <li>Iphone</li>
+                <li>Windows Phone</li>
+            </ul>
+            <h1>Mobile Manufacture</h1>
+            <ul>
+                <li>Samsung</li>
+                <li>HTC</li>
+                <li>Micromax</li>
+                <li>Apple</li>
+            </ul>
+
+
+        </div>
+    )
+} 
+
+export default List;
